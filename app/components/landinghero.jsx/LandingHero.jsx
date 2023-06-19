1cb487eb680137df2c98e39e6c7617b7449d3d68
Kike0013 <kique0013@hotmail.com>
2023-06-19 11:10:10 -0400

    [fix] escape char

--- app/components/landinghero.jsx/LandingHero.jsx
+++ app/components/landinghero.jsx/LandingHero.jsx
@@ -6,7 +6,7 @@ const LandingHero = () => {
             <div className="flex flex-col p-5 max-w-7xl my-96 text-center">
                 <h1 className="text-6xl mb-2">Keep track of your daily calorie limit!</h1>
                 <h3 className="text-xl mb-8">Tell us your maximum daily calorie intake and we will recommend the products that best suit your goals!</h3>
-                <Link href={"/products"} className="w-fit"><button className="text-2xl p-4 border border-d-violet-blue rounded-xl w-fit ms-5 hover:bg-[#10249F] hover:text-white">Let's begin</button></Link>
+                <Link href={"/products"} className="w-fit"><button className="text-2xl p-4 border border-d-violet-blue rounded-xl w-fit ms-5 hover:bg-[#10249F] hover:text-white">Let&apos;s begin</button></Link>
             </div>
         </div>
     )
